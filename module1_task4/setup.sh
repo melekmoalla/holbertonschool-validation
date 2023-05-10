5a0b1145c653fb99f7b9064bcfec10cdca99a858
melekmoalla <melekmoalla2468@gmail.com>
2023-05-10 19:38:57 +0100

    test somthing

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Set the version of Hugo to be installed
-HUGO_VERSION=0.88.1
+if [ ! -f "/tmp/installed.txt" ]; then
+  # Update the system
+  apt-get update -y
+  apt-get upgrade -y
 
-# Check if Hugo is installed and is the correct version
-if ! command -v hugo &> /dev/null || ! hugo version | grep -qF $HUGO_VERSION; then
-  echo "Installing Hugo $HUGO_VERSION"
-  # Download and install the correct version of Hugo
-  wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -O /tmp/hugo.tar.gz
-  tar -zxvf /tmp/hugo.tar.gz -C /usr/local/bin/
-  rm /tmp/hugo.tar.gz
+  # Install Hugo
+  apt-get install -y hugo
+
+  # Install Make
+  apt-get install -y make
+
+  # Create a flag file to indicate that hugo and make are installed
+  touch /tmp/installed.txt
 fi
 
 # Generate the website using Hugo
