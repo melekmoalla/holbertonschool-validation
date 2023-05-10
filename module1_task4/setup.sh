e1303be635d366154fba8935ba078f1a63ba61aa
melekmoalla <melekmoalla2468@gmail.com>
2023-05-10 19:18:58 +0100

    test module,4 another one

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-set -e
-
+# Check if Hugo is installed
 if ! command -v hugo >/dev/null 2>&1; then
+  # Install Hugo
   echo "Installing Hugo..."
-  sudo apt-get update
-  sudo apt-get install hugo -y
+  wget -qO- https://github.com/gohugoio/hugo/releases/download/v0.82.0/hugo_0.82.0_Linux-64bit.tar.gz | tar xvz -C /tmp
+  sudo mv /tmp/hugo /usr/local/bin/
 fi
 
+# Check if Make is installed
 if ! command -v make >/dev/null 2>&1; then
+  # Install Make
   echo "Installing Make..."
-  sudo apt-get update
-  sudo apt-get install make -y
+  sudo apt-get update && sudo apt-get install -y make
 fi
 
-echo "Building website..."
+# Build the website
+echo "Building the website..."
 make build
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+  echo "Build successful!"
+  exit 0
+else
+  echo "Build failed."
+  exit 1
+fi
