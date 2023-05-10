61f94644bbd5a42c5326dbd69136591b7ab85b0c
melekmoalla <melekmoalla2468@gmail.com>
2023-05-10 19:15:40 +0100

    test module,4 another one

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# Check if hugo is installed, and install it if not
-if ! command -v hugo &> /dev/null
-then
-    echo "hugo not found, installing..."
-    sudo apt-get update && sudo apt-get install hugo -y
+# Check if Docker daemon is running
+if ! systemctl status docker >/dev/null 2>&1; then
+    echo "Docker daemon is not running. Starting it..."
+    sudo systemctl start docker
 fi
 
-# Check if make is installed, and install it if not
-if ! command -v make &> /dev/null
-then
-    echo "make not found, installing..."
-    sudo apt-get update && sudo apt-get install make -y
-fi
+# Install Hugo
+sudo apt-get update
+sudo apt-get install -y hugo
 
-# Build the website using hugo
-echo "Building website..."
-if ! hugo -d dist
-then
-    echo "Error: failed to build website"
-    exit 1
-fi
-
-# Check if the website was generated successfully
-echo "Checking website..."
-if ! ls dist/ 2>/dev/null | grep -q 'index.html'
-then
-    echo "Error: website not generated"
-    exit 1
-fi
-
-echo "Website built successfully"
-exit 0
+# Generate the website
+hugo -d dist/
