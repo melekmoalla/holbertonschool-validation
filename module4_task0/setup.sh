de963a49aae19d5a535ea448044b1c28b819321e
melekmoalla <melekmoalla2468@gmail.com>
2023-05-25 22:11:44 +0100

    test

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Update package lists and install required packages
-sudo apt-get update
-sudo apt-get install -y hugo make
+# Update package lists and installation of required packages
+apt-get update && apt-get install -y hugo make npm curl
 
-# Change to the website directory
-cd module1_task3
+# download minimal version to use hugo with the template ananke
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
 
-# Run the build command
-make build
+# install hugo
+sudo apt install ./hugo.deb
 
+# remove file after the installation
+rm hugo.deb
+
+# Install markdownlint-cli and markdown-link-check
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
